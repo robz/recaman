f1137d3437b850e7b3205efd6713e472553479ee
robz <rjnevels@gmail.com>
2019-03-23 19:30:40 -0700

    fix bugs: start low and just oscillate

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const ctx = canvas.getContext('2d');
 const MID = canvas.height / 2;
 
 // ctx.arc(x, y, radius, startAngle, endAngle [, anticlockwise]);
-function drawCirc(from, to) {
-  const high = to < from;
+function drawCirc(from, to, high) {
   const radius = Math.abs(to - from) / 2;
   const start = to + (from - to) / 2;
   ctx.beginPath();
-  if (high) {
+  if (!high) {
     ctx.arc(start + 4, MID, radius, Math.PI, 0);
   } else {
     ctx.arc(start + 4, MID, radius, 0, Math.PI);
@@ -28,7 +27,7 @@ while (i < 600) {
   if (nn < 1 || s.has(nn)) {
     nn = n + i;
   }
-  drawCirc(n, nn);
+  drawCirc(n, nn, !!(i % 2));
   s.add(n);
   n = nn;
   i += 1;
